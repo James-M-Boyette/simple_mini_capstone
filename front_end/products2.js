6a32be2d60f849b1390e9a6f56353238fbee66a4
James Boyette <james.boyette@gmail.com>
2021-03-24 19:19:25 -0400

    Refresh

--- front_end/products2.js
+++ front_end/products2.js
@@ -4,7 +4,7 @@ var app = new Vue({
   el: "#app",
   data: {
     products: [],
-    message_1: "Hey from the .js file!!"
+    message_1: "Hey from the .js file!!",
   },
   methods: {
     showAllProducts: function () {
@@ -16,6 +16,20 @@ var app = new Vue({
     },
     showFirstProduct: function () {
       console.log("First product ... eventually");
+    },
+    createProduct: function () {
+      console.log("Making a product ...");
+      // mimic insomnia
+      // post /api/products
+      axios.post("http://localhost:3000/api/products", {
+        name:"pen",
+        price: 20,
+        description: "It writes upside down!",
+        image_url:""      // here, we're using snake_case, which wouldn't normally be accepted practice in js ... but it's what the key is written in, in our ruby db - we made a table with "image_url" as the column title (instead of "imageUrl"), so we need to make sure we can access it by using the proper ~spelling
+      }).then(response => {
+        console.log(response.data);
+      });
+      // Note: .then stipulates that js should do the stuff to the right *after* the previous action has completed ... which we need to make sure of bc sending requests and receiving responses from the internet takes time - much more time than accessing data locally on our PCs ...
     }
   }
 });
